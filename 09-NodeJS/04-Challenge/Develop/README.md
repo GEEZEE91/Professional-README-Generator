a2e1c627833fe8efa2b69bc4c8a0d5ae5a80d0fa
GEEZEE91 <GINA.ZIVKOVIC91@GMAIL.COM>
2022-10-17 10:47:17 -0400

    template md js changes

---
+++ 09-NodeJS/04-Challenge/Develop/README.md
@@ -0,0 +1,33 @@
+# jjj
+  
+  ## License 
+  ![Github license](http://img.shields.io/badge/license-GNU-blue.svg)
+ 
+  This project is license under GNU
+ 
+  ## Table of Contents
+  * [Description](#Description)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  
+  ## Description 
+  j'
+
+  ## Installation 
+      jjj
+
+  ## Usage 
+  j
+
+  ## Contributing 
+  
+
+  ## Tests
+  j
+
+  ## Questions
+  If you have any questions about this projects, please contact me directly at j. You can view more of my projects at https://github.com/hhh.
--- 09-NodeJS/04-Challenge/Develop/utils/generateMarkdown.js
+++ 09-NodeJS/04-Challenge/Develop/utils/generateMarkdown.js
@@ -1,28 +1,36 @@
 // function to generate markdown for README
 const generateMarkdown = data => {
   return `# ${data.title}
-  ![Github licence](http://img.shields.io/badge/license-${data.license}-blue.svg)
   
-  ## Description 
-  ${data.description}
+  ## License 
+  ![Github license](http://img.shields.io/badge/license-${data.license}-blue.svg)
+ 
+  This project is license under ${data.license}
+ 
   ## Table of Contents
-  * [Installation](#installation)
+  * [Description](#Description)
   * [Usage](#usage)
   * [License](#license)
   * [Contributing](#contributing)
   * [Tests](#tests)
   * [Questions](#questions)
   
+  
+  ## Description 
+  ${data.description}
+
   ## Installation 
-  ${data.install}
+      ${data.install}
+
   ## Usage 
   ${data.usage}
-  ## License 
-  This project is license under ${data.license}
+
   ## Contributing 
   ${data.contributors}
+
   ## Tests
   ${data.test}
+
   ## Questions
   If you have any questions about this projects, please contact me directly at ${data.email}. You can view more of my projects at https://github.com/${data.github}.
 `;
